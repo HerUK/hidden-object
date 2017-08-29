7c4b6fbd5af22d923c8b2dfc10d6dda039e099fb
Home <gjdnr2@gmail.com>
2017-08-29 23:55:10 +0900

    Second Commit

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataController : MonoBehaviour {
+
+    // Singleton class start
+    static GameObject _container;
+    static GameObject Container
+    {
+        get
+        {
+            return _container;
+        }
+    }
+
+    static DataController _instance;
+    public static DataController Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _container = new GameObject();
+                _container.name = "DataController";
+                _instance = _container.AddComponent(typeof(DataController)) as DataController;
+                DontDestroyOnLoad(_container);
+            }
+
+            return _instance;
+        }
+    }
+    // Singleton class end
+
+
+    public int Hint = 5;
+
+    public int HintPerHour = 1;
+
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text TextHint;
+    public Camera MainCamera;
+    public GameObject EffectSmoke;
+    public AudioClip SFXClick;
+
+
+    // Use this for initialization
+    void Start () {
+
+        TextHint.text = DataController.Instance.Hint.ToString();
+        StartCoroutine(StartConllectHint());
+	}
+	
+    IEnumerator StartConllectHint()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            DataController.Instance.Hint += DataController.Instance.HintPerHour;
+            TextHint.text = DataController.Instance.Hint.ToString();
+
+ 
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+           
+            Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                Debug.Log(hit.point);
+                Debug.DrawLine(ray.origin, hit.point, Color.red);
+                Instantiate(EffectSmoke, hit.point, EffectSmoke.transform.rotation);
+                MainCamera.gameObject.GetComponent<AudioSource>().PlayOneShot(SFXClick);
+            }
+
+        }
+
+    }
+
+   
+}
