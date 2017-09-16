063762ec87eea72699ae0a98e19eadf9d0194efd
DESKTOP-6RF5PDF\KYI <gjdnr2@naver.com>
2017-09-16 19:01:55 +0900

    그림 찾기에서 동그라미 추가

--- Assets/Scripts/HiddenObject.cs
+++ Assets/Scripts/HiddenObject.cs
@@ -1,12 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HiddenObject : MonoBehaviour {
 
+    bool IsFound = false;
+
     public void OnClickObject()
     {
-        Debug.Log("Find it!");
+        if (!IsFound)
+        {
+            IsFound = true;
+            Debug.Log("Find it!");
+            GameObject circle = new GameObject();
+            RectTransform rt = circle.AddComponent<RectTransform>();
+            Image img = circle.AddComponent<Image>();
+            circle.name = "Found";
+            circle.transform.parent = transform.parent;
+            circle.transform.SetSiblingIndex(1);
+            circle.transform.localPosition = new Vector3(0f, 0f, 0f);
+            circle.transform.localScale = new Vector3(1f, 1f, 1f);
+
+            img.sprite = Resources.Load<Sprite>("Sprites/Find");
+
+            Vector2 pos = rt.anchoredPosition;
+            pos.x = GetComponent<RectTransform>().anchoredPosition.x;
+            pos.y = GetComponent<RectTransform>().anchoredPosition.y;
+            rt.anchoredPosition = pos;
+
+            rt.sizeDelta = new Vector2(140f, 140f);
+
+        }
+
     }
 
 }
